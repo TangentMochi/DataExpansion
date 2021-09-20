932b9ee08b62f7af6b20ad876126456a63857d5d
Kikuchi Yu <yu180609@gmail.com>
2021-09-20 17:15:51 +0900

    init

--- dataexpansion/mysql_class.py
+++
@@ -1,9 +0,0 @@
-import dataexpansion.sql_class
-
-class MySQL_Ex(dataexpansion.sql_class.SQL_ABC) :
-    DataBaseHost = str()
-    DataBaseUser = str()
-    DataBasePass = str()
-    def __init__(self) -> None:
-        super().__init__()
-    pass
